65d69c870efe972c5bb9ba0c62149e52c8e8e80d
Victor Galan <victor.galan@liferay.com>
2017-10-03 15:47:38 +0200

    Better hybrid gallery

--- app/src/main/java/com/liferay/screenscordova/HybridGalleryActivity.java
+++ app/src/main/java/com/liferay/screenscordova/HybridGalleryActivity.java
@@ -3,10 +3,12 @@ package com.liferay.screenscordova;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import com.liferay.mobile.screens.util.LiferayLogger;
+import com.liferay.mobile.screens.web.WebListener;
 import com.liferay.mobile.screens.web.WebScreenlet;
 import com.liferay.mobile.screens.web.WebScreenletConfiguration;
 
-public class HybridGalleryActivity extends AppCompatActivity {
+public class HybridGalleryActivity extends AppCompatActivity implements WebListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,29 @@ public class HybridGalleryActivity extends AppCompatActivity {
 
 		WebScreenletConfiguration configuration = new WebScreenletConfiguration
 				.Builder("/web/guest/gallery")
+				.addRawCss(R.raw.gallery_portlet_css, "gallery_portlet_css")
+				.addRawJs(R.raw.gallery_portlet_js, "gallery_portlet_js")
 				.load();
 
 		webScreenlet.setWebScreenletConfiguration(configuration);
 
+		webScreenlet.setListener(this);
+
 		webScreenlet.load();
+	}
+
+	@Override
+	public void onPageLoaded(String url) {
+
+	}
+
+	@Override
+	public void onScriptMessageHandler(String namespace, String body) {
+		LiferayLogger.d(namespace + body);
+	}
+
+	@Override
+	public void error(Exception e, String userAction) {
+
 	}
 }
